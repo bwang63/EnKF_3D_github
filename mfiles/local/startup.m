961f0b267c3825a84bd83bb06c59b65f5b8f1a8b
bwang63 <Bin.Wang@dal.ca>
2022-04-17 20:22:43 -0300

    17-April-2022

--- mfiles/local/startup.m
+++ mfiles/local/startup.m
@@ -2,18 +2,9 @@
 %
 
 disp(['  Starting ' which(mfilename)])
-
-% disp(' Add netcdfAll-4.3.jar and mexcdf')
-% javaaddpath('../../matlab/netcdfAll-4.3.jar')
-% javaaddpath ('../../matlab/mexcdf/snctools/classes')
 addpath ../../matlab/mexcdf
 addpath('../../matlab/mexcdf/mexnc','-end')
 addpath('../../matlab/mexcdf/snctools','-end')
-
-% % add path for general toolboxes
-% add_ocean_toolboxes('../../matlab/toolbox/')
-% 
-% disp(' ')
 
 % add path to netcdf toolboxes
 disp(' Adding netcdf toolboxes')
