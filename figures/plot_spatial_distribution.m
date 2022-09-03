bcb02b7c59b367afe60dd2e31114c25d1113a44c
BinWang <56684814+bwang63@users.noreply.github.com>
2022-09-03 12:59:01 -0300

    Update plot_spatial_distribution.m

--- figures/plot_spatial_distribution.m
+++ figures/plot_spatial_distribution.m
@@ -4,9 +4,7 @@
 % assimilation run
 %
 % Modelled results of the truth and free runs are available at:
-% https://drive.google.com/drive/folders/1yp6HJzKfdrFtK4HTAKSbWA7o9CDKp_0r?usp=sharing
-% and
-% https://drive.google.com/drive/folders/1AKonErsFlrAFF1ZJlEa8UT1Kcde73DGx?usp=sharing
+% https://zenodo.org/record/6974184#.YxN5fuzMLJ8
 % 
 % Please search for '(edit)' to find settings that require changes to run
 % this script
